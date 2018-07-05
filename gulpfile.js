5b406c78143e69663c2937d30cb1a91de16486df
Александр <XoxlofAlexander@yandex.ru>
2018-07-05 21:03:09 +0300

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,85 @@
+var syntax        = 'styl';
+
+var gulp          = require('gulp'),
+		gutil         = require('gulp-util' ),
+		stylus          = require('gulp-stylus'),
+		browsersync   = require('browser-sync'),
+		concat        = require('gulp-concat'),
+		uglify        = require('gulp-uglify'),
+		cleancss      = require('gulp-clean-css'),
+		rename        = require('gulp-rename'),
+		autoprefixer  = require('gulp-autoprefixer'),
+		notify        = require("gulp-notify"),
+		rsync         = require('gulp-rsync'),
+		pug           = require('gulp-pug');
+
+gulp.task('browser-sync', function() {
+	browsersync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false,
+		// open: false,
+		// tunnel: true,
+		// tunnel: "projectname", //Demonstration page: http://projectname.localtunnel.me
+	})
+});
+
+gulp.task('pug', function() {
+	return gulp.src('app/pug/pages/*.pug')
+	.pipe(pug({
+		pretty: true
+	}))
+	.pipe(gulp.dest('app'))
+});
+
+gulp.task('styles', function() {
+	return gulp.src('app/styl/main.styl')
+	.pipe(stylus({ 'include css': true }).on("error", notify.onError()))
+	.pipe(rename({ suffix: '.min', prefix : '' }))
+	.pipe(autoprefixer(['last 15 versions']))
+	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
+	.pipe(gulp.dest('app/css'))
+	.pipe(browsersync.reload( {stream: true} ))
+});
+
+gulp.task('js', function() {
+	return gulp.src([
+		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/font-awesome/fontawesome-all.min.js',
+		'app/js/common.js', // Always at the end
+		])
+	.pipe(concat('scripts.min.js'))
+	// .pipe(uglify()) // Mifify js (opt.)
+	.pipe(gulp.dest('app/js'))
+	.pipe(browsersync.reload({ stream: true }))
+});
+
+gulp.task('build', function(){
+    return gulp.src("app/**")
+        .pipe(gulp.dest("dist"))
+});
+
+gulp.task('rsync', function() {
+	return gulp.src('app/**')
+	.pipe(rsync({
+		root: 'app/',
+		hostname: 'username@yousite.com',
+		destination: 'yousite/public_html/',
+		// include: ['*.htaccess'], // Includes files to deploy
+		exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excludes files from deploy
+		recursive: true,
+		archive: true,
+		silent: false,
+		compress: true
+	}))
+});
+
+gulp.task('watch', ['pug', 'styles', 'js', 'browser-sync'], function() {
+	gulp.watch('app/'+syntax+'/**/*.'+syntax+'', ['styles']);
+	gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['js']);
+	gulp.watch('app/pug/*/*.pug', ['pug']);
+	gulp.watch('app/*.html', browsersync.reload);
+});
+
+gulp.task('default', ['watch']);
